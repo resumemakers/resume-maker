b7b4700e0ab25bc1669c8edacf63df42d4d011a9
z4r4tu5tr4 <mendesxeduardo@gmail.com>
2017-01-08 01:15:47 -0200

    Comentário na função create

--- app/make.py
+++ app/make.py
@@ -4,6 +4,17 @@ from app.auxiliar import read_json, templates, mount_i18n
 
 
 def create(lang, template, json_file, output):
+    """
+    Função que cria o pdf baseado no html
+
+    :params:
+        - lang: Linguagem escolhida
+        - template: Template escolhido
+        - json_file: Arquivo a ser lido para gerar o cv
+        - output: arquivo de saida para o pdf
+
+    :returns: cv em pdf
+    """
     template_vars = read_json(json_file)
     template_vars.update(mount_i18n(lang))
 
