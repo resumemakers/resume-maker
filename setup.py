485560c45082e4ca8982cb47f9b8deff2365d97c
willian GL <williangldzn@gmail.com>
2018-10-05 21:01:39 -0300

    tox setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='resume-maker',
+      version='0.0.0',
+      description='A basic command-line resume generator to Python 3',
+      url='https://github.com/dunossauro/resume-maker',
+      author='Eduardo Mendes',
+      author_email='mendesxeduardo@gmail.com',
+      license='',
+      packages=[''],
+      zip_safe=False)
